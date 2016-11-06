54a938cafc775a5c083dc1d645e21ab6b9c18b56
sarunas8 <ramanauskassaras@gmail.com>
2016-11-06 22:04:41 +0200

    Main file init

---
+++ src/main/java/com/feikon/public/Website.java
@@ -0,0 +1,8 @@
+package com.feikon.public;
+
+public class Website {
+
+    public static void main(String[] args) {
+        /*This is entry point when application is executed as standalone jar(No servlet container or EE applicaiton server needed.)*/
+    }
+}
